f563585d88ab6b5bc12b1687d8cdc4a58cc7ec4d
Mike Alhayek <mike@crestapps.com>
2024-09-09 11:59:53 -0700

    Upgrade to OC 2.0

--- src/Modules/CrestApps.OrchardCore.Users/UserDisplayNameAdminMenu.cs
+++ src/Modules/CrestApps.OrchardCore.Users/UserDisplayNameAdminMenu.cs
@@ -1,11 +1,19 @@
 using CrestApps.OrchardCore.Users.Drivers;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Localization;
 using OrchardCore.Navigation;
 
 namespace CrestApps.OrchardCore.Users;
 
-public sealed class UserDisplayNameAdminMenu : INavigationProvider
+public sealed class UserDisplayNameAdminMenu : AdminNavigationProvider
 {
+    private static readonly RouteValueDictionary _routeValues = new()
+    {
+        { "area", "OrchardCore.Settings" },
+        { "groupId", DisplayNameSettingsDisplayDriver.GroupId },
+    };
+
+
     internal readonly IStringLocalizer S;
 
     public UserDisplayNameAdminMenu(IStringLocalizer<UserDisplayNameAdminMenu> stringLocalizer)
@@ -13,27 +21,21 @@ public sealed class UserDisplayNameAdminMenu : INavigationProvider
         S = stringLocalizer;
     }
 
-    public Task BuildNavigationAsync(string name, NavigationBuilder builder)
+    protected override ValueTask BuildAsync(NavigationBuilder builder)
     {
-        if (!NavigationHelper.IsAdminMenu(name))
-        {
-            return Task.CompletedTask;
-        }
-
         builder
             .Add(S["Configuration"], configuration => configuration
                 .Add(S["Settings"], settings => settings
                     .Add(S["User Display Name"], S["User Display Name"].PrefixPosition(), userDisplayName => userDisplayName
                         .AddClass("user-display-name")
                         .Id("userDisplayName")
-                        .Action("Index", "Admin", new { area = "OrchardCore.Settings", groupId = DisplayNameSettingsDisplayDriver.GroupId })
+                        .Action("Index", "Admin", _routeValues)
                         .Permission(UserPermissions.ManageDisplaySettings)
                         .LocalNav()
                     )
                 )
             );
 
-
-        return Task.CompletedTask;
+        return ValueTask.CompletedTask;
     }
 }
