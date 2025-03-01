506ff4b70831befbb120e9d2de1c7dd2a74be576
David Puplava <dpuplava@lavatg.com>
2025-02-28 23:51:43 -0600

    Enforce permissions for accessing Subscriber Dashboard

--- src/Modules/CrestApps.OrchardCore.Subscriptions/Controllers/DashboardController.cs
+++ src/Modules/CrestApps.OrchardCore.Subscriptions/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
+using CrestApps.OrchardCore.Subscriptions.Core;
 using CrestApps.OrchardCore.Subscriptions.Core.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OrchardCore.Admin;
 using OrchardCore.DisplayManagement;
@@ -9,13 +11,16 @@ namespace CrestApps.OrchardCore.Subscriptions.Controllers;
 [Admin]
 public class DashboardController : Controller
 {
+    private readonly IAuthorizationService _authorizationService;
     private readonly IDisplayManager<SubscriberDashboard> _displayManager;
     private readonly IUpdateModelAccessor _updateModelAccessor;
 
     public DashboardController(
+        IAuthorizationService authorizationService,
         IDisplayManager<SubscriberDashboard> displayManager,
         IUpdateModelAccessor updateModelAccessor)
     {
+        _authorizationService = authorizationService;
         _displayManager = displayManager;
         _updateModelAccessor = updateModelAccessor;
     }
@@ -23,6 +28,11 @@ public class DashboardController : Controller
     [Admin("subscription-dashboard")]
     public async Task<IActionResult> Index()
     {
+        if (!await _authorizationService.AuthorizeAsync(HttpContext.User, SubscriptionPermissions.AccessSubscriberDashboard))
+        {
+            return Forbid();
+        }
+
         var model = await _displayManager.BuildDisplayAsync(_updateModelAccessor.ModelUpdater);
 
         return View(model);
