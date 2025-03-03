2abe4a14f75b45ac83fa9d8f1fbbbba3996bbb40
Mike Alhayek <mike@crestapps.com>
2025-03-03 10:14:30 -0800

    Remove DeepSeek

--- src/Modules/CrestApps.OrchardCore.DeepSeek/Services/DeepSeekConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace CrestApps.OrchardCore.DeepSeek.Services;
-
-public static class DeepSeekConstants
-{
-    public const string ProviderTechnicalName = "DeepSeek";
-
-    public const string ImplementationName = "DeepSeek";
-}
