4700a9d42b96f6f1662a3dc7606afb7d87acfcfa
Mike Alhayek <mike@crestapps.com>
2025-06-10 10:56:56 -0700

    Defer migration to avoid deadlocks

--- src/Modules/CrestApps.OrchardCore.AI/Migrations/ProfileStoreMigrations.cs
+++ src/Modules/CrestApps.OrchardCore.AI/Migrations/ProfileStoreMigrations.cs
@@ -2,11 +2,13 @@ using CrestApps.OrchardCore.AI.Core.Models;
 using CrestApps.OrchardCore.AI.Models;
 using CrestApps.OrchardCore.Services;
 using Dapper;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OrchardCore.Data;
 using OrchardCore.Data.Migration;
 using OrchardCore.Documents;
 using OrchardCore.Environment.Shell;
+using OrchardCore.Environment.Shell.Scope;
 using YesSql;
 
 namespace CrestApps.OrchardCore.AI.Migrations;
@@ -14,26 +16,14 @@ namespace CrestApps.OrchardCore.AI.Migrations;
 [Obsolete("This class will be removed before the v1 is released.")]
 internal sealed class ProfileStoreMigrations : DataMigration
 {
-    private readonly INamedCatalog<AIProfile> _profilesCatalog;
-    private readonly IStore _store;
-    private readonly IDbConnectionAccessor _dbConnectionAccessor;
-    private readonly ShellSettings _shellSettings;
-    private readonly ILogger _logger;
+    private readonly ICatalog<AIProfile> _profilesCatalog;
     private readonly IDocumentManager<AIProfileDocument> _profileDocument;
 
     public ProfileStoreMigrations(
-        INamedCatalog<AIProfile> profilesCatalog,
-        IStore store,
-        IDbConnectionAccessor dbConnectionAccessor,
-        ShellSettings shellSettings,
-        ILogger<ProfileStoreMigrations> logger,
+        ICatalog<AIProfile> profilesCatalog,
         IDocumentManager<AIProfileDocument> profileDocument)
     {
         _profilesCatalog = profilesCatalog;
-        _store = store;
-        _dbConnectionAccessor = dbConnectionAccessor;
-        _shellSettings = shellSettings;
-        _logger = logger;
         _profileDocument = profileDocument;
     }
 
@@ -54,30 +44,37 @@ internal sealed class ProfileStoreMigrations : DataMigration
         return 2;
     }
 
-    public async Task<int> UpdateFrom1Async()
+    public int UpdateFrom1()
     {
-        var dialect = _store.Configuration.SqlDialect;
-
-        var table = $"{_store.Configuration.TablePrefix}{_shellSettings.GetDocumentTable()}";
-
-        var quotedTypeColumnName = dialect.QuoteForColumnName("Type");
-
-        var command = $"update {dialect.QuoteForTableName(table, _store.Configuration.Schema)} set {quotedTypeColumnName} = replace({quotedTypeColumnName}, 'CrestApps.OrchardCore.Models.ModelDocument', 'CrestApps.OrchardCore.Models.DictionaryDocument') where {quotedTypeColumnName} like 'CrestApps.OrchardCore.Models.ModelDocument%';";
-
-        await using var connection = _dbConnectionAccessor.CreateConnection();
-
-        try
+        ShellScope.AddDeferredTask(async scope =>
         {
-            await connection.OpenAsync();
-            await connection.ExecuteAsync(command);
-            await connection.CloseAsync();
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "An error occurred while updating indexing tasks Category to Content.");
+            var store = scope.ServiceProvider.GetRequiredService<IStore>();
+            var shellSettings = scope.ServiceProvider.GetRequiredService<ShellSettings>();
+            var dbConnectionAccessor = scope.ServiceProvider.GetRequiredService<IDbConnectionAccessor>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<ProfileStoreMigrations>>();
+            var dialect = store.Configuration.SqlDialect;
 
-            throw;
-        }
+            var table = $"{store.Configuration.TablePrefix}{shellSettings.GetDocumentTable()}";
+
+            var quotedTypeColumnName = dialect.QuoteForColumnName("Type");
+
+            var command = $"update {dialect.QuoteForTableName(table, store.Configuration.Schema)} set {quotedTypeColumnName} = replace({quotedTypeColumnName}, 'CrestApps.OrchardCore.Models.ModelDocument', 'CrestApps.OrchardCore.Models.DictionaryDocument') where {quotedTypeColumnName} like 'CrestApps.OrchardCore.Models.ModelDocument%';";
+
+            await using var connection = dbConnectionAccessor.CreateConnection();
+
+            try
+            {
+                await connection.OpenAsync();
+                await connection.ExecuteAsync(command);
+                await connection.CloseAsync();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "An error occurred while updating indexing tasks Category to Content.");
+
+                throw;
+            }
+        });
 
         return 2;
     }
