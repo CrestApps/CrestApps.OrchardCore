fb90d87929fc4c71a2932f1371d8e13080505c69
Mike Alhayek <mike@crestapps.com>
2025-10-28 20:59:58 -0700

    Add CatalogItemMigrations migration to migrate Id to ItemId

--- src/Modules/CrestApps.OrchardCore.AI/Migrations/AIProfileDefaultContextMigrations.cs
+++ src/Modules/CrestApps.OrchardCore.AI/Migrations/AIProfileDefaultContextMigrations.cs
@@ -1,9 +1,11 @@
 using CrestApps.OrchardCore.AI.Core.Models;
 using CrestApps.OrchardCore.AI.Models;
 using CrestApps.OrchardCore.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using OrchardCore.Data.Migration;
 using OrchardCore.Entities;
+using OrchardCore.Environment.Shell.Scope;
 
 namespace CrestApps.OrchardCore.AI.Migrations;
 
@@ -14,60 +16,57 @@ namespace CrestApps.OrchardCore.AI.Migrations;
 /// </summary>
 internal sealed class AIProfileDefaultContextMigrations : DataMigration
 {
-    private readonly INamedSourceCatalog<AIProfile> _profileCatalog;
-    private readonly DefaultAIOptions _defaultOptions;
-
-    public AIProfileDefaultContextMigrations(
-        INamedSourceCatalog<AIProfile> profileCatalog,
-        IOptions<DefaultAIOptions> defaultOptions)
+#pragma warning disable CA1822 // Mark members as static
+    public int Create()
+#pragma warning restore CA1822 // Mark members as static
     {
-        _profileCatalog = profileCatalog;
-        _defaultOptions = defaultOptions.Value;
-    }
-
-    public async Task<int> CreateAsync()
-    {
-        var profiles = await _profileCatalog.GetAllAsync();
-
-        foreach (var profile in profiles)
+        ShellScope.AddDeferredTask(async scope =>
         {
-            profile.Alter<AIProfileMetadata>(metadata =>
+            var profileCatalog = scope.ServiceProvider.GetRequiredService<INamedSourceCatalog<AIProfile>>();
+            var defaultOptions = scope.ServiceProvider.GetRequiredService<IOptions<DefaultAIOptions>>().Value;
+
+            var profiles = await profileCatalog.GetAllAsync();
+
+            foreach (var profile in profiles)
             {
-                if (!metadata.MaxTokens.HasValue)
+                profile.Alter<AIProfileMetadata>(metadata =>
                 {
-                    // Previously, we used 800 as the default max tokens if not specified.
-                    // We'll use 800 for backward compatibility while new profile will have no value set.
-                    metadata.MaxTokens = _defaultOptions.MaxOutputTokens ?? 800;
-                }
+                    if (!metadata.MaxTokens.HasValue)
+                    {
+                        // Previously, we used 800 as the default max tokens if not specified.
+                        // We'll use 800 for backward compatibility while new profile will have no value set.
+                        metadata.MaxTokens = defaultOptions.MaxOutputTokens ?? 800;
+                    }
 
-                if (!metadata.PresencePenalty.HasValue)
-                {
-                    metadata.PresencePenalty = _defaultOptions.PresencePenalty;
-                }
+                    if (!metadata.PresencePenalty.HasValue)
+                    {
+                        metadata.PresencePenalty = defaultOptions.PresencePenalty;
+                    }
 
-                if (!metadata.FrequencyPenalty.HasValue)
-                {
-                    metadata.FrequencyPenalty = _defaultOptions.FrequencyPenalty;
-                }
+                    if (!metadata.FrequencyPenalty.HasValue)
+                    {
+                        metadata.FrequencyPenalty = defaultOptions.FrequencyPenalty;
+                    }
 
-                if (!metadata.Temperature.HasValue)
-                {
-                    metadata.Temperature = _defaultOptions.Temperature;
-                }
+                    if (!metadata.Temperature.HasValue)
+                    {
+                        metadata.Temperature = defaultOptions.Temperature;
+                    }
 
-                if (!metadata.TopP.HasValue)
-                {
-                    metadata.TopP = _defaultOptions.TopP;
-                }
+                    if (!metadata.TopP.HasValue)
+                    {
+                        metadata.TopP = defaultOptions.TopP;
+                    }
 
-                if (!metadata.PastMessagesCount.HasValue)
-                {
-                    metadata.PastMessagesCount = _defaultOptions.PastMessagesCount;
-                }
-            });
+                    if (!metadata.PastMessagesCount.HasValue)
+                    {
+                        metadata.PastMessagesCount = defaultOptions.PastMessagesCount;
+                    }
+                });
 
-            await _profileCatalog.UpdateAsync(profile);
-        }
+                await profileCatalog.UpdateAsync(profile);
+            }
+        });
 
         return 1;
     }
