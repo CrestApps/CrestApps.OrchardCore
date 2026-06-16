2fcd65249cc75a78e6a7a107e7dea867f14f0208
Mike Alhayek <mike@crestapps.com>
2026-06-16 09:44:34 -0700

    Fix Phone Number Migration (#480)

--- tests/CrestApps.OrchardCore.Tests/Modules/Omnichannel/Managements/Migrations/ContactMethodMigrationsTests.cs
+++ tests/CrestApps.OrchardCore.Tests/Modules/Omnichannel/Managements/Migrations/ContactMethodMigrationsTests.cs
@@ -45,6 +45,41 @@ public sealed class ContactMethodMigrationsTests
     }
 
     [Fact]
+    public void TryMigratePhoneNumberField_WhenLegacyPossibleCanadianPhoneNumberExists_StoresPhoneFieldProperties()
+    {
+        // Arrange
+        var innerContentItem = new JsonObject
+        {
+            ["ContentType"] = OmnichannelConstants.ContentTypes.PhoneNumber,
+            [OmnichannelConstants.ContentParts.PhoneNumberInfo] = new JsonObject
+            {
+                ["Number"] = new JsonObject
+                {
+                    ["Text"] = "(778) 552-8744",
+                },
+                ["Type"] = new JsonObject
+                {
+                    ["Text"] = "Home",
+                },
+            },
+        };
+
+        // Act
+        var migrated = ContactMethodMigrations.TryMigratePhoneNumberField(innerContentItem, _phoneNumberService);
+
+        // Assert
+        Assert.True(migrated);
+
+        var phoneNumberInfoPart = Assert.IsType<JsonObject>(innerContentItem[OmnichannelConstants.ContentParts.PhoneNumberInfo]);
+        var numberNode = Assert.IsType<JsonObject>(phoneNumberInfoPart["Number"]);
+
+        Assert.Null(numberNode["Text"]);
+        Assert.Equal("+17785528744", numberNode["PhoneNumber"]?.GetValue<string>());
+        Assert.Equal("CA", numberNode["CountryCode"]?.GetValue<string>());
+        Assert.Equal("7785528744", numberNode["NationalNumber"]?.GetValue<string>());
+    }
+
+    [Fact]
     public void TryMigratePhoneNumberField_WhenPhoneNumberWasAlreadyMigrated_ReturnsFalse()
     {
         // Arrange
